d600ff3f79aa5bd57f45f72276581015619199d9
PhieAsh <yaoshiu@qq.com>
2022-10-07 01:17:51 +0800

    removed dbus-python

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author="Phie Ash",
     description="A script to automatically paint images on Splatoon3 through a Linux system emulating a Switch Pro controller.",
     install_requires=[
-        "opencv-python", "nxbt[dbus-python]", "aioconsole", "dbus-python"
+        "opencv-python", "nxbt[dbus-python]", "aioconsole"
     ]
 )
