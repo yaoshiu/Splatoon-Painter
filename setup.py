32fe15e4c1d927ca2b604121cccb07a741bf7c4f
yaoshiu <yaoshiu@qq.com>
2022-10-12 13:05:33 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(
     name="splatoon-painter",
-    include_package_data=True,
-    version="1.0",
     author="Phie Ash",
     description="A script to automatically paint images on Splatoon3 through a Linux system emulating a Switch Pro controller.",
     install_requires=[
